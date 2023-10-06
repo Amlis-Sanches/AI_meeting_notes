9d4c96c1c25b1de361ad633a4841e2535d40f5d0
Amlis-Sanches <nathanielchorn@nchsande.com>
2023-10-06 14:34:35 -0500

    Eddited the message

--- Core.py
+++ Core.py
@@ -53,11 +53,7 @@ def abstract_summary_extraction(transcription):
     response = openai.Completion.create(
         model="text-davinci-003",
         temperature=0,
-        prompt="You are a highly skilled AI trained in language comprehension and summarization. "
-            "I would like you to read the following text and summarize it into a concise abstract paragraph. "
-            "Aim to retain the most important points, providing a coherent and readable summary that could help a person "
-            "understand the main points of the discussion without needing to read the entire text. Please avoid unnecessary "
-            "details or tangential points.\n\n" + transcription
+        prompt="You are a highly skilled AI trained in language comprehension and summarization. I would like you to read the following text and summarize it into a concise abstract paragraph. Aim to retain the most important points, providing a coherent and readable summary that could help a person understand the main points of the discussion without needing to read the entire text. Please avoid unnecessary details or tangential points.\n\n" + transcription
     )
     return response['choices'][0]['text']
 
@@ -68,10 +64,7 @@ def key_points_extraction(transcription):
     response = openai.Completion.create(
         model="text-davinci-003",
         temperature=0,
-        prompt="You are a proficient AI with a specialty in distilling information into key points." 
-            "Based on the following text, identify and list the main points that were discussed or brought up." 
-            "These should be the most important ideas, findings, or topics that are crucial to the essence of the discussion." 
-            "Your goal is to provide a list that someone could read to quickly understand what was talked about.\n\n" + transcription
+        prompt="You are a proficient AI with a specialty in distilling information into key points. Based on the following text, identify and list the main points that were discussed or brought up. These should be the most important ideas, findings, or topics that are crucial to the essence of the discussion. Your goal is to provide a list that someone could read to quickly understand what was talked about.\n\n" + transcription
     )
     return response['choices'][0]['text']
 
@@ -80,10 +73,7 @@ def action_item_extraction(transcription):
     response = openai.Completion.create(
         model="text-davinci-003",
         temperature=0,
-        prompt="You are an AI expert in analyzing conversations and extracting action items."
-            "Please review the text and identify any tasks, assignments, or actions that were agreed upon or mentioned as needing to be done." 
-            "These could be tasks assigned to specific individuals, or general actions that the group has decided to take." 
-            "Please list these action items clearly and concisely. \n\n" + transcription
+        prompt="You are an AI expert in analyzing conversations and extracting action items. Please review the text and identify any tasks, assignments, or actions that were agreed upon or mentioned as needing to be done. These could be tasks assigned to specific individuals, or general actions that the group has decided to take. Please list these action items clearly and concisely. \n\n" + transcription
     )
     return response['choices'][0]['text']
 
@@ -92,9 +82,7 @@ def sentiment_analysis(transcription):
     response = openai.Completion.create(
         model="text-davinci-003",
         temperature=0,
-        prompt="As an AI with expertise in language and emotion analysis, your task is to analyze the sentiment of the following text."
-            "Please consider the overall tone of the discussion, the emotion conveyed by the language used, and the context in which words and phrases are used." 
-            "Indicate whether the sentiment is generally positive, negative, or neutral, and provide brief explanations for your analysis where possible.\n\n" + transcription
+        prompt="As an AI with expertise in language and emotion analysis, your task is to analyze the sentiment of the following text. Please consider the overall tone of the discussion, the emotion conveyed by the language used, and the context in which words and phrases are used. Indicate whether the sentiment is generally positive, negative, or neutral, and provide brief explanations for your analysis where possible.\n\n" + transcription
     )
     return response['choices'][0]['text']
 
